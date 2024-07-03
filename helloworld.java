74dd085ad408ca6b1c6e97184bd52cedf789ac43
prshanthi04 <99384706+prshanthi04@users.noreply.github.com>
2024-07-03 19:50:01 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Students");
-      System.out.println("Welcome to Jenkins class");
+      System.out.println("Welcome to Jenkins class!!!");
     }  
 } 
